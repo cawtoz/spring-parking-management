6be11166bbabe137f3be003c0d450a3f16a8445e
cawtoz <cawtoz.dev@gmail.com>
2024-11-04 18:08:03 -0500

    Initial commit

---
+++ src/main/java/com/github/cawtoz/parking/SpringParkingManagementApplication.java
@@ -0,0 +1,13 @@
+package com.github.cawtoz.parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringParkingManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringParkingManagementApplication.class, args);
+	}
+
+}
